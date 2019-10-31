9548b5b3e46ddada2bf4e4d2cb9217d1f86380e1
Rostyk <rkanafotskyy@gmail.com>
2019-10-31 17:56:28 +0200

    Script added, update to dokerfile, tip jenkinsfile update

---
+++ components-publish.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+publishDirName="components"
+isChanged=$(
+    # Difference in 2 last commits
+    git diff --name-only HEAD^ HEAD |
+    # Get only that starts with components so only component folder
+    grep "^${publishDirName}/" |
+    # Get first one
+    head -1 |
+    # Cut all after /
+    cut -d / -f 1
+)
+
+if [[ ${isChanged} == ${publishDirName} ]]
+then
+    git checkout RS-516-automate-version-increment
+    npm version patch  -m "Components Bump"
+    # git push
+    # npm publish components/
+    exit 0
+fi
+echo "components not published"
